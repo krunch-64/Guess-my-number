839c0022484288299ff2051a2246d40726cb52cc
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-01-11 10:16:56 +0100

    js random num

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,41 @@
-    function startGame() {
-        $("btn-primmary").click( function () {
-            console.log("test bouton")
-        })
-    }
+// Fonction commencer la partie
+function startGame() {
+    $("btn-primmary").click(function () {
+        console.log("test bouton")
+    })
+};
+// Fonction random
+function getRandomNumber() {
+    let max = 0;
 
-
-    $(function () {
-        startGame()
+    $('select').on('change', function() {
+        if($('select').val() == 1){
+            max = 50;
+            // console.log('0 - 50');
+        }
+        else if($('select').val() == 2){
+            max = 100;
+            // console.log('0 - 100');
+        }
+        else if($('select').val() == 3){
+            max = 250;
+            // console.log('0 - 250');
+        };
+    });
+    $("#btn-start").click(function () {
+        return Math.floor(Math.random() * max);
         
-})
+        // console.log(max);
+    })
+    
+
+
+};
+
+// Fonction ready
+$(function () {
+
+    startGame();
+    getRandomNumber();
+
+});
